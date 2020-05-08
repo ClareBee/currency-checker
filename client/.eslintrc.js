3f49cb477befe9200dc7f0de9169393a266f4eb0
Clare <chblackburne@gmail.com>
2020-05-08 12:27:26 +0100

    eslint config

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:jsx-a11y/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    quotes: [2, "double"],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
--- server.js
+++ server.js
@@ -3,23 +3,25 @@ const fetch = require("node-fetch");
 const path = require("path");
 const cors = require("cors");
 
+// TODO: move to env file
 const API_URL = "http://data.fixer.io/api/";
 const API_KEY = "14fdb7f80578d3cb6775222e27f9df1b";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// move to .env file
 const corsOptions = {
   origin: "http://localhost:8080",
 };
 app.use(express.static(path.join(__dirname, "client/dist")));
+app.listen(port, () => console.log(`Server Running on ${port}!`));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/dist/index.html"));
 });
 
-app.listen(port, () => console.log(`Server Running on ${port}!`));
-
+// TODO: move to routes folder
 app.get("/api/latest", cors(corsOptions), (req, res, next) => {
   const url = `${API_URL}latest?access_key=${API_KEY}`;
 
